d32ae15f579deb23580eac5c6114042565ec328f
Tornike Vakhtangidze <tornikevakhtangidze@gmail.com>
2024-05-21 23:15:11 +0400

    test 6?

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -14,8 +14,8 @@ export class ProductsService {
 
   sum(arr: number[]): number {
     let s: number = 0;
-    if (arr === undefined) {  }
-    else { 
+    if (arr === undefined) {
+    } else {
       for (let i of arr) {
         s += i;
       }
@@ -88,34 +88,24 @@ export class ProductsService {
   }
 
   addToCart(idCart: number) {
+    if (this.cartArray.id == undefined) {
+      console.log('undefine fixing');
+      this.cartArray = { id: [0], quantity: [0] };
+    }
 
-    const index = this.cartArray.id.indexOf(idCart);
+    if (this.cartArray.id.includes(idCart)) {
+      console.log('include worked');
+      for (let i = 0; i < this.cartArray.id.length; i++) {
+        if (this.cartArray.id[i] === idCart) {
+          this.cartArray.quantity[i] += 1;
+        }
+      }
+    } else {
+      console.log('adding new item to cart');
+      this.cartArray.id.push(idCart);
+      this.cartArray.quantity.push(1);
+    }
 
-if (index !== -1) {
-    // If the id exists, increase the quantity
-    this.cartArray.quantity[index] += 1;
-    console.log('Increased quantity for item:', idCart);
-} else {
-    // If the id doesn't exist, add it to the cartArray with quantity 1
-    this.cartArray.id.push(idCart);
-    this.cartArray.quantity.push(1);
-    console.log('Added new item to cart:', idCart);
-}
-
-      // if (this.cartArray.id.includes(idCart)) {
-      //   console.log('include worked')
-      //   for (let i = 0; i < this.cartArray.id.length; i++) {
-      //     if (this.cartArray.id[i] === idCart) {
-      //       this.cartArray.quantity[i] += 1;
-      //     }
-      //   }
-      // }
-      // else {
-      //   console.log('adding new item to cart')
-      //   this.cartArray.id.push(idCart);
-      //   this.cartArray.quantity.push(1);
-      // }
-    
     localStorage.setItem('cart', JSON.stringify(this.cartArray));
     this.cartNumber$.next(this.sum(this.cartArray.quantity));
     this.cArray$.next(this.cartArray);
@@ -151,15 +141,15 @@ if (index !== -1) {
           this.cArray$.next(this.cartArray);
         }
 
+        if (this.cartArray.id == undefined) {
+          console.log('undefine fixing');
+          this.cartArray = { id: [0], quantity: [0] };
+        }
+
         this.cartProducts$.next(
-          this.AllProducts$.getValue().products.filter((p) =>
-          {
-            if (this.cartArray && this.cartArray.id) { 
-              this.cartArray.id.includes(p.id)
-            }
-            
-            }
-          )
+          this.AllProducts$.getValue().products.filter((p) => {
+            this.cartArray.id.includes(p.id);
+          })
         );
       });
   }
